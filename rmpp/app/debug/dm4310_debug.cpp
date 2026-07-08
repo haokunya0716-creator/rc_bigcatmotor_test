91ec27242bb3c3123d02e61a32b21ccb02cab6c7
userName <userEmail>
2026-07-08 13:41:08 +0800

    pid差不多调整了一下

--- rmpp/app/debug/dm4310_debug.cpp
+++ rmpp/app/debug/dm4310_debug.cpp
@@ -1,20 +1,28 @@
+#include <cstdio>
+#include "usart.h" // 包含usart头文件以调用 huart6
+
 #include "motor/DM4310.hpp"
 
 static constexpr float TARGET_ANGLE_DEG = 0.0f;
 static constexpr float SPEED_FF_DEG_S = 0.0f;
 
+float angle_ref = 0;
+
 static PID::config_t dm_angle_pid = {
-    .kp = (80.0f * rpm) / (10.0f * deg),
-    .ki = 0 * default_unit,
+    .kp = (15.0f * rpm) / (10.0f * deg),
+    .ki = 0.0 * default_unit,
     .kd = 0 * default_unit,
+    .max_i = 50.0f * rpm,
     .max_out = 100.0f * rpm,
+
 };
 
 static PID::config_t dm_speed_pid = {
-    .kp = (2.0f * Nm) / (100.0f * rpm),
-    .ki = 0 * default_unit,
+    .kp = (13.0f * Nm) / (100.0f * rpm),
+    .ki = 0.0 * default_unit,
     .kd = 0 * default_unit,
-    .max_out = 6.0f * Nm,
+    .max_i = 2.0f * Nm,
+    .max_out = 2.0f * Nm,
 };
 
 DM4310 dm_motor({
@@ -29,7 +37,7 @@ DM4310 dm_motor({
     .is_invert = false,
     .offset = 0 * deg,
     .is_limit = true,
-    .limit_min = -90 * deg,
+    .limit_min = -60 * deg,
     .limit_max = 90 * deg,
 
     .control_mode = Motor::ANGLE_SPEED_MODE,
@@ -46,19 +54,34 @@ void setup()
 
 void loop()
 {
-    dm_motor.SetAngle(TARGET_ANGLE_DEG * deg, SPEED_FF_DEG_S * deg_s);
+    //dm_motor.SetAngle(angle_ref * deg, SPEED_FF_DEG_S * deg_s);
     dm_motor.OnLoop();
     dm_motor.SendCanCmd();
 }
+
+// ---------------- 重定向 printf 到 UART6 ----------------
+extern "C" int _write(int file, char *ptr, int len)
+{
+    HAL_UART_Transmit(&huart6, (uint8_t *)ptr, len, HAL_MAX_DELAY);
+    return len;
+}
+// --------------------------------------------------------
 
 extern "C" void rmpp_main()
 {
     setup();
 
-    BSP::Dwt dwt;
+    BSP::Dwt dwt_loop;  // 给控制循环用的定时器
+    BSP::Dwt dwt_print; // 给串口打印用的定时器
+
     while (true) {
-        if (dwt.PollTimeout(1 * ms)) {
+        if (dwt_loop.PollTimeout(1 * ms)) {
             loop();
+        }
+
+        if (dwt_print.PollTimeout(50 * ms)) {
+            printf("%f,%f\n", dm_motor.angle.measure.value,dm_motor.angle.ref.value);
+            //printf("%f,%f\n", dm_motor.speed.ref.value, dm_motor.speed.measure.value);
         }
     }
 }
